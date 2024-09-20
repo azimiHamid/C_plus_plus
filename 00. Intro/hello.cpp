08b7c50328e45d0a5d9602cc732d78998216514a
azimiHamid <azimihamid2003@gmail.com>
2024-09-20 16:48:00 +0500

    update intro

--- 00. Intro/hello.cpp
+++ 00. Intro/hello.cpp
@@ -4,6 +4,8 @@ using namespace std;
 int main() {
 
     cout << "Hello World!" << endl;
+    cout << "My name is \"Hamid\" and I am a software developer.\n";
+    cout << "This is my First C++ code!";
 
     return 0;
 }
